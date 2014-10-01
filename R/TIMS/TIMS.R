07bf9033e542dd3ac9bb06f7d9b0aaf1afe74e37
Yuriy Oleksyshyn <yuraolex@gmail.com>
2014-10-01 14:32:57 +0300

    Added plots

--- R/TIMS/TIMS.R
+++ R/TIMS/TIMS.R
@@ -3,8 +3,8 @@ library(e1071)
 data <- read.table("data.txt", fileEncoding="latin1")
 dataVect <- unlist(data, use.names = FALSE)
 freq <- table(dataVect)
-freq
-barplot(freq)         # apply the barplot function
+freqPlot <- plot(freq)
+barPlot <- barplot(freq)         # apply the barplot function
 
 standart <- sd(dataVect)
 variance <- standart * standart
@@ -16,3 +16,6 @@ median <- median(dataVect)
 decLocations <- quantile(dataVect, probs = seq(0.1,0.9,by=0.1))
 scope <- max(dataVect) - min(dataVect)
 ac <- moment(dataVect, order=3, center=TRUE) / (moment(dataVect, order=2, center=TRUE) ^ (2/3))
+ex <- moment(dataVect, order=4, center=TRUE) / (moment(dataVect, order=2, center=TRUE) ^ 2) - 3
+ecdf = ecdf(dataVect)
+funcPlot <- plot(ecdf)
