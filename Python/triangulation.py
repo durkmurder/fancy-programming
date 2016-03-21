4ee3ea812dac805b175caf6189027bc04b6020e2
Yura Oleksyshyn <VolodymyrVrublevskyi@MacBook-Air-Volodymyr.local>
2016-03-21 22:50:57 +0200

    Fixed triangulation to rectangular mesh.

--- Python/triangulation.py
+++ Python/triangulation.py
@@ -8,7 +8,7 @@ import math
 
 
 nY = 5
-nX = nY
+nX = 5
 
 hX = 1. / nX
 hY = 1. / nY
@@ -17,22 +17,14 @@ print(hX)
 
 xArr = np.tile([i * hX for i in xrange(0, nX + 1)], nY + 1)
 yArr = []
-for i in xrange(0, nX + 1):
-    # np.repeat([hY * i], nY + 1)) #np.asarray([0., 0., 0., 0.5, 0.5, 0.5, 1.0, 1.0, 1.0])
-    for j in xrange(0, nY + 1):
-        yArr.append(hY * i)
+[[ yArr.append(hY * i) for j in xrange(0, nX + 1)] for i in xrange(0, nY + 1)]
 
-print yArr
+#for i in xrange(0, len(xArr)):
+#    print (i, xArr[i], yArr[i])
+
 triangles = []
 
 print len(xArr), len(yArr)
-
-'''
-[0, 1, 4],
-[1, 2, 5],
-[3, 4, 7],
-[4, 5, 8]
-'''
 
 for i in xrange(0, nY):
     for j in xrange(0, nX):
@@ -42,16 +34,12 @@ for i in xrange(0, nY):
         index = j + (i + 1) * (nX + 1)
         triangles.append([index, j + i * (nX + 1), index + 1]);
 
-# Rather than create a Triangulation object, can simply pass x, y and triangles
-# arrays to triplot directly.  It would be better to use a Triangulation object
-# if the same triangulation was to be used more than once to save duplicated
-# calculations.
 plt.figure()
 plt.gca().set_aspect('equal')
 plt.triplot(xArr, yArr, triangles, 'go-')
 plt.title('triplot of user-specified triangulation')
-plt.xlim(-0.5, 1.5)
-plt.ylim(-0.5, 1.5)
+plt.xlim(-0.1, 1.1)
+plt.ylim(-0.1, 1.1)
 plt.xlabel('Longitude (degrees)')
 plt.ylabel('Latitude (degrees)')
 
