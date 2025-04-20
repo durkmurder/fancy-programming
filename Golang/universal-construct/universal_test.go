fbbd84ba61074ac8b24059afaa4d8bc906a4f423
Yurii Oleksyshyn <yuraolex@gmail.com>
2025-04-20 11:59:19 +0300

    Updated godoc

--- Golang/universal-construct/universal_test.go
+++ Golang/universal-construct/universal_test.go
@@ -8,6 +8,8 @@ import (
 
 const ops = 10
 
+// TestLFUniversal emulates an execution where multiple goroutines try to modify seq object at the same time.
+// A lock-free universal construction is used to produce a lineriazable execution.
 func TestLFUniversal(t *testing.T) {
 	universal := NewLFUniversal[uint32](0)
 
@@ -20,24 +22,28 @@ func TestLFUniversal(t *testing.T) {
 	for i := range nThreads {
 		go func(threadID uint32) {
 			defer done.Done()
+			// signal that we have started
 			started.Done()
+			// wait for coordinated start
 			ready.Wait()
+
 			for range ops {
 				_ = universal.Apply(threadID, func(u uint32) uint32 {
 					return u + (threadID + 1)
 				})
 			}
-
-			//fmt.Printf("Thread ID: %d, Decided: %v\n", threadID, response)
 		}(uint32(i))
 	}
 
+	// wait for all workers to start
 	started.Wait()
-	ready.Done()
+	// signal about coordinated start
 	done.Wait()
 	fmt.Printf("lock-free accum value: %d\n", universal.Apply(0, func(u uint32) uint32 { return u }))
 }
 
+// TestWFUniversal emulates an execution where multiple goroutines try to modify seq object at the same time.
+// A wait-free universal construction is used to produce a lineriazable execution.
 func TestWFUniversal(t *testing.T) {
 	universal := NewWFUniversal[uint32](0)
 
@@ -50,18 +56,22 @@ func TestWFUniversal(t *testing.T) {
 	for i := range nThreads {
 		go func(threadID uint32) {
 			defer done.Done()
+			// signal that we have started
 			started.Done()
+			// wait for coordinated start
 			ready.Wait()
+
 			for range ops {
 				_ = universal.Apply(threadID, func(u uint32) uint32 {
 					return u + (threadID + 1)
 				})
 			}
-			//fmt.Printf("Thread ID: %d, Decided: %v\n", threadID, response)
 		}(uint32(i))
 	}
 
+	// wait for all workers to start
 	started.Wait()
+	// signal about coordinated start
 	ready.Done()
 	done.Wait()
 	fmt.Printf("wait-free accum value: %d\n", universal.Apply(0, func(u uint32) uint32 { return u }))
